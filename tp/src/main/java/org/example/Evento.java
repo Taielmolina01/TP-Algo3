19d0dbd7ba4c67733e30e9621fd427e009f50ad9
Tai <tamolina@fi.uba.ar>
2023-04-12 13:40:13 -0300

    Constructor evento

--- tp/src/main/java/org/example/Evento.java
+++ tp/src/main/java/org/example/Evento.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class Evento {
+    private String nombre;
+    private String descripcion;
+
+    public Evento(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public String getDescripcion() {
+        return this.descripcion;
+    }
 
 }
