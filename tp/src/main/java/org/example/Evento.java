7ece307c9fd0713badf1062606cc29ee9859ad47
Tai <tamolina@fi.uba.ar>
2023-04-13 20:14:50 -0300

    Chequear si existe evento funciona (ponele)

--- tp/src/main/java/org/example/Evento.java
+++ tp/src/main/java/org/example/Evento.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import java.time.*;
 import java.lang.String;
 
@@ -10,26 +9,25 @@ public class Evento {
     private LocalDateTime fechaInicio;
     private LocalDateTime fechaFin; // Fin del evento sin contar sus repeticiones, NO es la fecha en donde terminan las repeticiones.
     private LocalDateTime fechaFinalDefinitivo; // Fecha en la que terminan las repeticiones del evento.
-    private LocalTime duracion;
+    private Duration duracion;
     private Integer repeticiones;
     private String[] frecuencia;
 
-    private void constructorDefault(String nombre, String descripcion, LocalDateTime fechaInicio, LocalTime duracion) {
+    private void constructorDefault(String nombre, String descripcion, LocalDateTime fechaInicio, Duration duracion) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.fechaInicio = fechaInicio;
+        this.duracion = duracion;
         this.calcularFechaFin(duracion, fechaInicio);
     }
 
-    private LocalDateTime calcularFechaFin(LocalTime duracion, LocalDateTime fechaInicio) {
-        int hsDuracion = duracion.getHour();
-        int minsDuracion = duracion.getMinute();
-        int segsDuracion = duracion.getSecond();
-        fechaFin = fechaInicio.plusHours(hsDuracion).plusMinutes(minsDuracion).plusSeconds(segsDuracion);
+    private LocalDateTime calcularFechaFin(Duration duracion, LocalDateTime fechaInicio) {
+        fechaFin = fechaInicio.plusHours(duracion.toHours());
         return fechaFin;
     }
 
-    /* Frecuencia debe ser un array de tamaño 2, la primera posicion debe tener un indicador de si la frecuencia es diaria
+    /*
+        Frecuencia debe ser un array de tamaño 2, la primera posicion debe tener un indicador de si la frecuencia es diaria
         semanal, mensual o anual representado por una letra
         "D": diaria
         "S": semanal
@@ -38,16 +36,18 @@ public class Evento {
         En la segunda posicion se debe indicar cada cuantos dias/semanas/meses/años se produce el evento
     */
 
-    private boolean hayEvento(LocalDateTime diaAAnalizar) {
+    public boolean hayEvento(LocalDateTime diaAAnalizar) {
         if (diaAAnalizar.isBefore(this.fechaInicio) || diaAAnalizar.isAfter(this.fechaFinalDefinitivo)) {
             return false;
         } else {
-            /* Puede haber evento porque está entre el rango en el que inicio el evento y cuando termina
-            deberia analizar como hago para ir avanzando desde la fecha inicial hasta al fechaAAnalizar SIN PASARME,
-            y devolver true si en la fechaAAnalizar hubo evento
+            /*
+                Puede haber evento porque está entre el rango en el que inicio el evento y cuando termina
+                deberia analizar como hago para ir avanzando desde la fecha inicial hasta al fechaAAnalizar SIN PASARME,
+                y devolver true si en la fechaAAnalizar hubo evento
             */
             String funcionAUtilizar = ""; // Ver q onda esto
             Long multiplicador = Long.parseLong("1");
+            LocalDateTime dia = this.fechaInicio;
             switch (this.frecuencia[0]) {
                 case "D":
                     funcionAUtilizar = "PD"; multiplicador = Long.parseLong(this.frecuencia[1]);
@@ -63,28 +63,28 @@ public class Evento {
                     break;
             }
             if (funcionAUtilizar.equals("PD")) { // Repito codigo a lo loco
-                for (LocalDateTime dia = this.fechaInicio; dia.isBefore(this.fechaFinalDefinitivo); dia = dia.plusDays(multiplicador)) {
+                while (dia.isBefore(this.fechaFinalDefinitivo)) {
                     if (diaAAnalizar.isEqual(dia) || (diaAAnalizar.isAfter(dia) && diaAAnalizar.isBefore(this.calcularFechaFin(this.duracion, dia)))) { // deberia hacer que si diaAAnalizar isBetween dia y dia + duracion
                         return true;
                     }
+                    dia = dia.plusDays(multiplicador);
                 }
-                return false;
             }
             if (funcionAUtilizar.equals("PM")) {
-                for (LocalDateTime dia = this.fechaInicio; dia.isBefore(this.fechaFinalDefinitivo); dia = dia.plusMonths(multiplicador)) {
+                while (dia.isBefore(this.fechaFinalDefinitivo)) {
                     if (diaAAnalizar.isEqual(dia) || (diaAAnalizar.isAfter(dia) && diaAAnalizar.isBefore(this.calcularFechaFin(this.duracion, dia)))) {
                         return true;
                     }
+                    dia = dia.plusMonths(multiplicador);
                 }
-                return false;
             }
             if (funcionAUtilizar.equals("PY")) {
-                for (LocalDateTime dia = this.fechaInicio; dia.isBefore(this.fechaFinalDefinitivo); dia = dia.plusYears(multiplicador)) {
+                while (dia.isBefore(this.fechaFinalDefinitivo)) {
                     if (diaAAnalizar.isEqual(dia) || (diaAAnalizar.isAfter(dia) && diaAAnalizar.isBefore(this.calcularFechaFin(this.duracion, dia)))) {
                         return true;
                     }
                 }
-                return false;
+                dia = dia.plusYears(multiplicador);
             }
             return false;
         }
@@ -92,36 +92,26 @@ public class Evento {
 
 
     // Constructor si no se repite el evento nunca.
-    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, LocalTime duracion) {
+    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, Duration duracion) {
         this.constructorDefault(nombre, descripcion, fechaInicio, duracion);
         this.fechaFinalDefinitivo = this.fechaFin;
     }
 
     // Constructor si se repite el evento dada la fecha de fin.
-    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, LocalTime duracion, LocalDateTime fechaFinalDefinitivo,
-                  String[] frecuencia) {
+    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, Duration duracion,
+                  LocalDateTime fechaFinalDefinitivo, String[] frecuencia) {
         this.constructorDefault(nombre, descripcion, fechaInicio, duracion);
         this.fechaFinalDefinitivo = fechaFinalDefinitivo;
-        if (this.validarFrecuencia(frecuencia)) {
-            this.frecuencia = frecuencia;
-        }
+        this.frecuencia = frecuencia;
     }
 
 
     // Constructor si se repite el evento dada las veces que se va a repetir el evento.
-    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, LocalTime duracion, Integer ocurrencias, String[] frecuencia) {
+    public Evento(String nombre, String descripcion, LocalDateTime fechaInicio, Duration duracion,
+                  Integer ocurrencias, String[] frecuencia) {
         this.constructorDefault(nombre, descripcion, fechaInicio, duracion);
-        if (this.validarFrecuencia(frecuencia)) {
-            this.frecuencia = frecuencia;
-        }
+        this.frecuencia = frecuencia;
     }
-
-
-
-
-
-
-
 
     public String getNombre() {
         return this.nombre;
@@ -143,7 +133,7 @@ public class Evento {
         this.fechaInicio = fechaInicio;
     }
 
-    public void modificarDuracion(LocalTime duracion) {
+    public void modificarDuracion(Duration duracion) {
         this.fechaFin = this.calcularFechaFin(duracion, this.fechaInicio);
     }
 
--- tp/src/test/java/org/example/EventoTest.java
+++ tp/src/test/java/org/example/EventoTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
@@ -11,7 +13,7 @@ public class EventoTest {
     @Test
     public void testGetters() {
         LocalDateTime fechaCumple = LocalDateTime.of(2001, 12, 22, 0, 0, 0);
-        LocalTime duracion = LocalTime.of(23, 59);
+        Duration duracion = Duration.ofHours(24);
         Evento evento = new Evento("Cumpleaños", "Cumple Facu 22 años", fechaCumple, duracion);
 
         assertEquals("Cumpleaños", evento.getNombre());
@@ -23,5 +25,17 @@ public class EventoTest {
         assertEquals("Cumpleañitos", evento.getNombre());
         assertEquals("Cumple Facu 21 años", evento.getDescripcion());
          */
+    }
+
+    @Test
+    public void testFechas() {
+        LocalDateTime fechaInicio = LocalDateTime.of(2001, 12, 22, 0, 0, 0);
+        LocalDateTime fechaFinal = LocalDateTime.of(2002, 12, 22, 0, 0, 0);
+        Duration duracion = Duration.ofHours(26);
+        String[] frecuencia = {"D", "3"};
+        Evento evento = new Evento("Evento1", "Este es el evento1", fechaInicio, duracion, fechaFinal, frecuencia);
+        LocalDateTime fechaAAnalizar = LocalDateTime.of(2001, 12, 25, 0, 0, 0);
+
+        assertTrue(evento.hayEvento(fechaAAnalizar));
     }
 }
