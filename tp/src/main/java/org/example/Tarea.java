68c1fe9d8ad48429c7fb151fd3625d56c65b9d13
TheKKriger <49736578+TheKKriger@users.noreply.github.com>
2023-04-13 22:40:37 -0300

    Se agregaron atributos básicos de Tarea y la función toogleTarea().

--- tp/src/main/java/org/example/Tarea.java
+++ tp/src/main/java/org/example/Tarea.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Tarea {
+
+    private String titulo;
+    private String descripcion;
+
+    private boolean completada;
+
+    public void toddleTarea() {
+        completada = !completada;
+    }
 }
