621fca831420d8a7c53ac5bcdc40b8253c4791ee
Tai <tamolina@fi.uba.ar>
2023-04-18 14:28:27 -0300

    frecuencia con enums

--- tp/src/main/java/org/example/Main.java
+++ tp/src/main/java/org/example/Main.java
@@ -1,22 +1,10 @@
 package org.example;
 
-
-import java.time.DayOfWeek;
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime diaInicio = LocalDateTime.parse("2018-01-10T11:25");
-        LocalDateTime diaFin = LocalDateTime.parse("2019-01-10T11:25");
-        String nombreEvento = "Cumple Luqui";
-        String descripcionEvento = "Autista hijo de puta";
-        DayOfWeek[] diasRepeticion = new DayOfWeek[]{};
-        String[] hola = {"hola"};
-        Duration duracion = Duration.ofHours(16);
-        frecuenciaMensual frecuencia = new frecuenciaMensual(diasRepeticion, 2);
-        Evento evento = new Evento(nombreEvento, descripcionEvento,
-                diaInicio, duracion, false, diaFin, frecuencia);
-        System.out.println(frecuencia.getTipoFrecuencia());
+        LocalDateTime diaInicio = LocalDateTime.parse("2024-03-15T11:25");
+        System.out.println(diaInicio.plusMonths(1));
     }
 }
---
+++ tp/src/main/java/org/example/frecuencia.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public enum frecuencia {
+
+    DIARIA {
+        public LocalDateTime getProximaFecha(LocalDateTime fechaInicial, Integer frecuenciaDiaria) {
+            return fechaInicial.plusDays(frecuenciaDiaria);
+        }
+    },
+
+    SEMANAL {
+
+        /*diasSemana debe estar en orden segun el ordinal*/
+        public LocalDateTime getProximaFecha(LocalDateTime fechaInicial, DayOfWeek[] diasSemana,
+                                             Integer frecuenciaSemanal) {
+            DayOfWeek diaActual = fechaInicial.getDayOfWeek();
+            LocalDateTime fechaProxima = null;
+            // [MONDAY, THURSDAY]
+            for (int i = 0; i < diasSemana.length; i++) {
+                var dia = diasSemana[i];
+                if (diaActual == dia) {
+                    if (i != diasSemana.length - 1) { // ok
+                        fechaProxima = fechaInicial.plusDays(diasSemana[i+1].getValue() - diaActual.getValue());
+                    } else {
+                        fechaProxima = fechaInicial.plusDays((diasSemana[0].getValue() + diaActual.getValue()) % 8 + (frecuenciaSemanal - 1));
+                    }
+                }
+            }
+            return fechaProxima;
+        }
+    },
+
+    MENSUAL {
+        public LocalDateTime getProximaFecha(LocalDateTime fechaInicial, Integer frecuenciaMensual) {
+            return fechaInicial.plusMonths(frecuenciaMensual);
+        }
+    },
+
+    ANUAL {
+        public LocalDateTime getProximaFecha(LocalDateTime fechaInicial, Integer frecuenciaAnual) {
+            return fechaInicial.plusYears(frecuenciaAnual);
+        }
+    };
+
+
+}
