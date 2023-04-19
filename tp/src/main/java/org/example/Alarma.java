567edf4acc309944c3b959d5d98f3408e23ad65d
TheKKriger <49736578+TheKKriger@users.noreply.github.com>
2023-04-19 20:25:07 -0300

    Se agregó el constructor de la clase Alarma

--- tp/src/main/java/org/example/Alarma.java
+++ tp/src/main/java/org/example/Alarma.java
@@ -1,11 +1,21 @@
 package org.example;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
 
 public class Alarma {
+
+    private LocalDateTime fechaAbsoluta;
+    private TemporalAmount tiempoRelativo;
+    private LocalDateTime fechaDisparador;
+
+    public Alarma(LocalDateTime fechaAbsoluta, TemporalAmount tiempoRelativo) {
+        this.fechaAbsoluta = fechaAbsoluta;
+        this.tiempoRelativo = tiempoRelativo;
+        this.fechaDisparador = fechaAbsoluta.minus(tiempoRelativo);
+    }
 
     public void dispararAlarma() {
 
     }
-
 }
