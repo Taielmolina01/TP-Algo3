39e23c34e83dbb8d74c3344812b36c763605ba30
TheKKriger <49736578+TheKKriger@users.noreply.github.com>
2023-04-22 15:18:28 -0300

    Se agregaron las funciones necesarias para crear un evento con  el BuilderEvento

--- tp/src/main/java/org/example/BuilderEvento.java
+++ tp/src/main/java/org/example/BuilderEvento.java
@@ -1,6 +1,41 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class BuilderEvento {
 
+    private Evento evento;
 
+    public BuilderEvento() {
+
+    }
+
+    public void setNombre(String nombre) {
+        evento.modificarNombre(nombre);
+    }
+
+    public void setDescripcion(String descripcion) {
+        evento.modificarDescripcion(descripcion);
+    }
+
+    public void setFechaInicial(LocalDateTime fechaInicial) {
+        evento.modificarFechaInicio(fechaInicial);
+    }
+
+    public void setDuracion(Duration duracion, boolean todoElDia) {
+        evento.modificarDuracion(duracion);
+    }
+
+    public void setFrecuencia(Frecuencia frecuencia) {
+        evento.modificarFrecuencia(frecuencia);
+    }
+
+    public void setRepeticion(int repeticion) {
+        evento.modificarOcurrencias(repeticion);
+    }
+
+    public Evento getResultado() {
+        return this.evento;
+    }
 }
