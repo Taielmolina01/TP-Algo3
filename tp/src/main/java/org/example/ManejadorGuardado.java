73d51d5fa1e7263c5bd1f9f5530314c7be1b1179
Tai <tamolina@fi.uba.ar>
2023-05-21 14:59:34 -0300

    Correcciones diego listas

--- tp/src/main/java/org/example/ManejadorGuardado.java
+++ tp/src/main/java/org/example/ManejadorGuardado.java
@@ -9,24 +9,28 @@ public class ManejadorGuardado {
 
     protected final PrintStreamMock salida;
     private final PrintStream salidaReal;
+    private final String rutaArchivoGuardado = "MiCalendario.txt";
+    private File archivoGuardado;
+
 
     public ManejadorGuardado() {
         this.salidaReal = System.out;
         this.salida = new PrintStreamMock(this.salidaReal);
+        this.crearArchivoGuardado();
     }
 
-    protected void guardarEstado(String rutaArchivo, Calendario calendario) {
+    protected void guardarEstado(Calendario calendario) {
         try {
-            FileOutputStream archivoDestino = new FileOutputStream(rutaArchivo);
+            FileOutputStream archivoDestino = new FileOutputStream(this.rutaArchivoGuardado);
             calendario.serializar(archivoDestino);
         } catch (FileNotFoundException e) {
             this.salida.println("El archivo de guardado no existe.");
         }
     }
 
-    protected Calendario recuperarEstado(String rutaArchivo) {
+    protected Calendario recuperarEstado() {
         try {
-            FileInputStream archivo = new FileInputStream(rutaArchivo);
+            FileInputStream archivo = new FileInputStream(this.rutaArchivoGuardado);
             return (new Calendario()).deserializar(archivo);
         } catch (FileNotFoundException e ) {
             this.salida.println("El archivo de recuperado no existe.");
@@ -34,11 +38,20 @@ public class ManejadorGuardado {
         }
     }
 
-    protected void borrarEstadoGuardado(String rutaArchivo) {
-        try (BufferedWriter bf = Files.newBufferedWriter(Path.of(rutaArchivo),
+    protected void borrarEstadoGuardado() {
+        try (BufferedWriter bf = Files.newBufferedWriter(Path.of(this.rutaArchivoGuardado),
                 StandardOpenOption.TRUNCATE_EXISTING)) {
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void crearArchivoGuardado() {
+        this.archivoGuardado = new File(rutaArchivoGuardado);
+        try {
+            this.archivoGuardado.createNewFile();
+        } catch (IOException e) {
+            //
         }
     }
 }
